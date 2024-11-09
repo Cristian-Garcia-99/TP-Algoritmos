e9fa5dab5189842d9f92e2181fbe42fcd7962959
Garcia Cristian <garcia.cristian.arg@gmail.com>
2024-11-09 15:47:32 -0300

    Autogenerar ID

--- Salon de eventos/Salon de eventos/Empleado.cs
+++ Salon de eventos/Salon de eventos/Empleado.cs
@@ -13,6 +13,10 @@ namespace Salon_de_eventos
         protected string cargo;
         protected string legajo;
         protected int sueldo;
+        
+        //Atributo autogenerativo
+        protected static int id;
+        protected int iden;
 
         public Empleado(string nombre, string dni, string cargo, string legajo, int sueldo)
         {
@@ -21,8 +25,12 @@ namespace Salon_de_eventos
             this.cargo = cargo;
             this.legajo = legajo;
             this.sueldo = sueldo;
+            
+            //Codigo autogenerativo
+            id++;
+            this.iden = id;     
         }
-
+        public int ID { get { return iden; } }
         public string Nombre { get { return nombre; } }
         public string Dni { get { return dni; } }
         public string Cargo { get { return cargo; } }
